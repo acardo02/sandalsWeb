c6a3ab04d671004f5f5d15a29490ad387d5405fa
acardo02 <00037820@uca.edu.sv>
2025-12-21 15:48:20 -0600

    feat: adding CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.connection import init_db
-from app.routes import users_routes, auth_routes, products_routes, order_routes
+from app.routes import users_routes, auth_routes, products_routes, order_routes, upload_routes
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      
+    allow_credentials=True,     
+    allow_methods=["*"],        
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
     await init_db()
 
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
 app.include_router(users_routes.router, prefix="/users", tags=["Users"])
 app.include_router(products_routes.router, prefix="/products", tags=["Products"])
 app.include_router(order_routes.router, prefix="/orders", tags=["Orders"])
+app.include_router(upload_routes.router, prefix="/upload", tags=["Uploads"])
